40e62632616a43949656cc1309d12244b9a7c0d7
Shafeen M <sodl02@gmail.com>
2015-07-20 16:51:48 -0400

    Created initial files for a sample FillInTheBlanks Web Application

---
+++ WebApps/FillInTheBlanks/scripts/index.js
@@ -0,0 +1,51 @@
+/**
+ * Created by smahmud on 7/20/2015.
+ */
+
+var BLANK_TOKEN = "::blank::";
+
+function genSentenceMarkup(sentenceFragments) {
+    var $sentence = $("<li></li>");
+
+    sentenceFragments.forEach(function (fragment) {
+        console.log(fragment);
+
+        if(fragment == BLANK_TOKEN) {
+            $sentence.append($("<input type=text/>"));
+        } else {
+            $sentence.append(fragment);
+        }
+        $sentence.append(" ");
+    });
+
+    // append the generated sentence to the list
+    $("#sentence-list").append($sentence);
+}
+
+function genAndAppendSentence() {
+    var inputStr = $("#gen-input").val();
+    if(inputStr == "") {
+        alert("String cannot be empty");
+        return;
+    }
+
+    var sentenceFragments = inputStr.split(" ");
+    genSentenceMarkup(sentenceFragments);
+}
+
+
+$(document).ready(function() {
+
+    $("#gen-btn").on("click", function(e) {
+        genAndAppendSentence();
+    });
+
+    $("#insert-blank-btn").on("click", function(e) {
+        $("#gen-input").val($("#gen-input").val() + BLANK_TOKEN);
+    });
+});
+
+
+
+
+
