2d204a9ad9c973b6519c5bdb51619fb9af28a60b
Shafeen M <sodl02@gmail.com>
2016-02-04 23:10:34 -0500

    Component tags all created for TaxCalculator (reactjs-version). Need to add event handlers and figure out how calculation will work.

---
+++ WebApps/TaxCalculator/reactjs-version/taxCalculator.js
@@ -0,0 +1,54 @@
+
+var LabeledSelector = React.createClass({
+    render: function () {
+        var optionNodes = this.props.optionData.map(function (option) {
+            return (
+                <option value={option.value}>{option.innerText}</option>
+            );
+        });
+        return (
+            <div>
+                <label for={this.props.id}>{this.props.label}</label>
+                <select id={this.props.id}>
+                    {optionNodes}
+                </select>
+            </div>
+        );
+    }
+});
+
+var LabeledNumberInput = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <label for={this.props.id}>{this.props.label}</label>
+                <input id={this.props.id} type="number" disabled={this.props.disabled=="y"}/>
+            </div>
+        );
+    }
+});
+
+// TODO: complete this
+ReactDOM.render(
+    <div>
+        <h2>Calculation</h2>
+        <LabeledSelector id="filing-status" label="Filing Status"
+            optionData={[
+            { value: "single", innerText: "Single" },
+            { value: "headOfHouseHold", innerText: "Head of Household" },
+            { value: "marriedFilingJointly", innerText: "Married Filing Jointly" },
+            { value: "marriedSeparateFiling", innerText: "Married Filing Separately" }
+        ]}/>
+        <LabeledNumberInput id="gross-income" label="Gross Income $" />
+        <LabeledNumberInput id="fed-deductions" label="Fed Deductions $" />
+
+        <hr/>
+
+        <h2>Summary</h2>
+        <LabeledNumberInput id="fed-tax-owed" label="Fed Income Taxes Owed $" disabled="y" />
+        <LabeledNumberInput id="fica-tax-owed" label="FICA/Social Security Owed $" disabled="y" />
+        <LabeledNumberInput id="net-income" label="Net Income $" disabled="y" />
+
+    </div>,
+    document.getElementById("container")
+);
