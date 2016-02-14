a39b910fb1939c0d2b61a03d6f74f2c8a7b91436
Shafeen M <sodl02@gmail.com>
2016-02-13 23:55:59 -0500

    updated reactJS version of TaxCalculator extracted react components used for the calculation portion into a new react component <TaxCalculator/>

--- WebApps/TaxCalculator/reactjs-version/taxCalculator.js
+++ WebApps/TaxCalculator/reactjs-version/taxCalculator.js
@@ -1,4 +1,5 @@
 
+
 var LabeledSelector = React.createClass({
     render: function () {
         var optionNodes = this.props.optionData.map(function (option) {
@@ -8,7 +9,7 @@ var LabeledSelector = React.createClass({
         });
         return (
             <div>
-                <label for={this.props.id}>{this.props.label}</label>
+                <label htmlFor={this.props.id}>{this.props.label}</label>
                 <select id={this.props.id}>
                     {optionNodes}
                 </select>
@@ -18,11 +19,39 @@ var LabeledSelector = React.createClass({
 });
 
 var LabeledNumberInput = React.createClass({
+    handleChange: function (event) {
+        console.log(event.target.value);
+    },
+
     render: function() {
         return (
             <div>
-                <label for={this.props.id}>{this.props.label}</label>
-                <input id={this.props.id} type="number" disabled={this.props.disabled=="y"}/>
+                <label htmlFor={this.props.id}>{this.props.label}</label>
+                <input id={this.props.id}
+                       type="number"
+                       disabled={this.props.disabled=="y"}
+                       onChange={this.props.onChange}
+                       onClick={this.props.onClick}/>
+            </div>
+        );
+    }
+});
+
+var TaxCalculator = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h2>Calculation</h2>
+                <LabeledSelector id="filing-status" label="Filing Status"
+                    optionData={[
+                    { value: "single", innerText: "Single" },
+                    { value: "headOfHouseHold", innerText: "Head of Household" },
+                    { value: "marriedFilingJointly", innerText: "Married Filing Jointly" },
+                    { value: "marriedSeparateFiling", innerText: "Married Filing Separately" }
+                ]}/>
+                <LabeledNumberInput id="gross-income" label="Gross Income $" />
+                <LabeledNumberInput id="fed-deductions" label="Fed Deductions $" />
+                <button id="calculate-btn">Calculate</button>
             </div>
         );
     }
@@ -30,22 +59,11 @@ var LabeledNumberInput = React.createClass({
 
 // TODO: complete this
 // TODO: read up on Flux architectural pattern
+// TODO: read up on Redux architectural pattern
 ReactDOM.render(
     <div>
-        <h2>Calculation</h2>
-        <LabeledSelector id="filing-status" label="Filing Status"
-            optionData={[
-            { value: "single", innerText: "Single" },
-            { value: "headOfHouseHold", innerText: "Head of Household" },
-            { value: "marriedFilingJointly", innerText: "Married Filing Jointly" },
-            { value: "marriedSeparateFiling", innerText: "Married Filing Separately" }
-        ]}/>
-        <LabeledNumberInput id="gross-income" label="Gross Income $" />
-        <LabeledNumberInput id="fed-deductions" label="Fed Deductions $" />
-        <button id="calculate-btn">Calculate</button>
-
+        <TaxCalculator />
         <hr/>
-
         <h2>Summary</h2>
         <LabeledNumberInput id="fed-income-taxes-owed" label="Fed Income Taxes Owed $" disabled="y" />
         <LabeledNumberInput id="fica-soc-taxes-owed" label="FICA/Social Security Owed $" disabled="y" />
