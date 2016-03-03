a83779c94ee5b81766d191d4df9e907b1f63c6e1
Shafeen M <sodl02@gmail.com>
2016-03-02 23:35:18 -0500

    adding more Junit tests for Person class

--- Java/GsonTest/src/test/java/TestPerson.java
+++ Java/GsonTest/src/test/java/TestPerson.java
@@ -3,7 +3,7 @@ import com.google.gson.Gson;
 
 public class TestPerson {
     @Test
-    public void testJsonOutput() {
+    public void testGsonOutput() {
         Gson gson = new Gson();
         Assert.assertNotSame("Should not be the same string.",
                 gson.toJson(new Person("Fred Flintstone", 42, "Married")),
@@ -11,10 +11,20 @@ public class TestPerson {
     }
 
     @Test
-    public void testJsonOutput2() {
+    public void testGsonOutput2() {
         Gson gson = new Gson();
         Assert.assertEquals("Should be the same string.",
                 gson.toJson(new Person("Betty Rubble", 42, "Married")),
                 "{\"name\":\"Betty Rubble\",\"age\":42,\"maritalStatus\":\"Married\"}");
+    }
+
+    @Test
+    public void testGetters() {
+        String name = "John Doe", maritalStatus = "Single";
+        int age = 100;
+        Person person = new Person(name, age, maritalStatus);
+        Assert.assertEquals("Values don't match!", person.getName(), name);
+        Assert.assertEquals("Values don't match!", person.getAge(), age);
+        Assert.assertEquals("Values don't match!", person.getMaritalStatus(), maritalStatus);
     }
 }
