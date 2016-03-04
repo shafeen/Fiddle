aef3d91995d558cbac3126115bce90d2c72d36c6
Shafeen M <sodl02@gmail.com>
2016-03-03 23:57:04 -0500

    Added composer.json PHPUnit dependency and a basic test

---
+++ PHP/tests/TemplateTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class TemplateTest extends PHPUnit_Framework_TestCase
+{
+    // TODO: run this test correctly to verify that PHPUnit is working
+    public function testBasicMustacheTemplate() {
+        $mustacheFileLoader = new Mustache_Engine([
+            'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../templates/')
+        ]);
+        $templateFileName = 'hello'; // .mustache appended automatically
+        $values = ['planet' => 'World!'];
+        $this->assertEquals('Hello World!', $mustacheFileLoader->render($templateFileName, $values))
+    }
+}
