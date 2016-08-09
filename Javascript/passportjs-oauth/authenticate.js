df6300e196044737401b6957dfd0d5726f423b48
shafeen <sodl02@gmail.com>
2016-08-09 01:40:18 -0400

    updated passportjs-oauth to use oauth2 for the sample code STILL DOES NOT WORK CORRECTLY

--- Javascript/passportjs-oauth/authenticate.js
+++ Javascript/passportjs-oauth/authenticate.js
@@ -2,7 +2,7 @@ var express  = require('express');
 var router = express.Router();
 
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 // TODO: THIS IS STILL NON-FUNCTIONAL!!!
 
@@ -13,8 +13,8 @@ var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 //   invoke a callback with a user object.
 var secrets = require('./secrets.json'); // create this file containing your google api info
 passport.use(new GoogleStrategy({
-        consumerKey: secrets.GOOGLE_CONSUMER_KEY,
-        consumerSecret: secrets.GOOGLE_CONSUMER_SECRET,
+        clientID: secrets.GOOGLE_CONSUMER_KEY,
+        clientSecret: secrets.GOOGLE_CONSUMER_SECRET,
         callbackURL: "/authenticate/google/callback/"
     },
     function(token, tokenSecret, profile, done) {
@@ -26,30 +26,29 @@ passport.use(new GoogleStrategy({
 
 // TODO: serve the log-in page here
 router.get('/login/', function (req, res) {
-    res.send('log in with Google here');
+    res.sendFile('index.html', {root: './public'});
 });
 
-// GET /auth/google
+// GET /authenticate/google
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in Google authentication will involve redirecting
 //   the user to google.com.  After authorization, Google will redirect the user
 //   back to this application at /auth/google/callback
 // TODO: update the "scope" parameter
 router.get('/google/',
-    passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' })
+    passport.authenticate('google', { scope: ['profile', 'email'] })
 );
 
-// GET /auth/google/callback
+// GET /authenticate/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-router.get('/google/callback',
-    passport.authenticate('google', { failureRedirect: '/authenticate/login' }),
-    function(req, res) {
-        console.log('got here after passport authentication');
-        res.redirect('/google/success/');
-    }
+router.get('/google/callback/',
+    passport.authenticate('google', {
+        successRedirect: '/authenticate/google/success/',
+        failureRedirect: '/authenticate/login/'
+    })
 );
 
 // TODO: invoke this route after a successful login
