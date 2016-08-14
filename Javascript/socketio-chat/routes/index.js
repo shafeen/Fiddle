8ad9463308cbee7d0a6ac1a15e0e5f335d1f13b8
Shafeen M <sodl02@gmail.com>
2016-08-13 23:17:41 -0400

    update socketio-chat: fixed a small naming issue

--- Javascript/socketio-chat/routes/index.js
+++ Javascript/socketio-chat/routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var subapp = express.Router();
+var router = express.Router();
 
 
 /* GET home page. */
-subapp.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express (sub app)' });
 });
 
-subapp.get('/chat/', function (req, res, next) {
+router.get('/chat/', function (req, res, next) {
   res.render('chat', {
     title: 'Socket.IO chat'
   });
 });
 
-module.exports = subapp;
+module.exports = router;
