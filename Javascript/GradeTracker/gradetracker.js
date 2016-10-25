7808a341423cd497422ae1f04e10ae3a4bc42e24
Shafeen M <sodl02@gmail.com>
2016-10-25 00:57:37 -0400

    updated the prototype for the GradeTracker

--- Javascript/GradeTracker/gradetracker.js
+++ Javascript/GradeTracker/gradetracker.js
@@ -1,12 +1,11 @@
 angular.module("gradetrackerApp", [])
 .controller("GradeTrackerCtrl", function () {
     var gradetracker = this;
-    gradetracker.totalClasses = 4;
+
     gradetracker.grades = [
         {name: 'Psych 101', grade: 'B', bestPossible: 'A', worstPossible: 'C'},
         {name: 'Physics 240', grade: 'C', bestPossible: 'B', worstPossible: 'D'}
     ];
-
     gradetracker.newClass = {};
     gradetracker.addNewClass = function () {
         gradetracker.grades.push({
@@ -18,7 +17,18 @@ angular.module("gradetrackerApp", [])
         gradetracker.newClass = {};
     };
 
-
+    gradetracker.items = [];
+    gradetracker.newItem = {};
+    gradetracker.addNewItem = function () {
+        gradetracker.items.push({
+            name: gradetracker.newItem.name,
+            type: gradetracker.newItem.type,
+            weight: gradetracker.newItem.weight,
+            achieved: gradetracker.newItem.achieved,
+            maximum: gradetracker.newItem.maximum
+        });
+        gradetracker.newItem = {};
+    };
 
 
 });
