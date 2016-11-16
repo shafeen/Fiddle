a43256c07eed5233522e752758ccb04281a3662e
Shafeen M <sodl02@gmail.com>
2016-11-15 23:59:46 -0500

    created sample project passportjs-local (incomplete)

---
+++ Javascript/passportjs-local/config/passport.js
@@ -0,0 +1,7 @@
+var LocalStrategy = require('passport-local').Strategy;
+
+var User = require('../models/user.model');
+
+module.exports(function (passport) {
+    // TODO: complete this to configure passportJS
+});
