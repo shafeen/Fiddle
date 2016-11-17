c926badb55a341c101da4b64ac792d33d980c81c
Shafeen M <sodl02@gmail.com>
2016-11-16 23:58:31 -0500

    updated passportjs-local: configuring signup to use a LocalStrategy

--- Javascript/passportjs-local/config/passport.js
+++ Javascript/passportjs-local/config/passport.js
@@ -3,5 +3,33 @@ var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user.model');
 
 module.exports(function (passport) {
-    // TODO: complete this to configure passportJS
+
+
+    // passport session setup
+    // ----------------------
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        })
+    });
+
+    // local signup strategy
+    // ---------------------
+    // if the strategies weren't named, it would default to "local"
+    passport.use('local-signup', new LocalStrategy({
+            // local strategy uses username and password by default
+            // -> override with email and password
+            usernameField: 'email',
+            passwordfield: 'password',
+            passReqToCallback: true
+        },
+        function(req, email, password, done) {
+            // TODO: complete the rest to configure passportJS
+        }
+    ));
+
+
 });
