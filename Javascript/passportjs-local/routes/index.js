b47ff85a63efa5e2fdf0963e553e6c8554f247c2
Shafeen M <sodl02@gmail.com>
2016-11-19 01:02:45 -0500

    updated passportjs-local: protected '/profile' page and added 'logout' you have to be logged in to view the page

--- Javascript/passportjs-local/routes/index.js
+++ Javascript/passportjs-local/routes/index.js
@@ -13,10 +13,26 @@ router.get('/signup', function (req, res) {
     res.render('signup', {title: 'New User', signupMsg: req.flash('signupMessage')});
 });
 
-router.get('/profile', function (req, res) {
+//we want this protected so you have to be logged in to visit
+router.get('/profile', isLoggedIn, function (req, res) {
     // TODO: update this route to a better profile page
     res.render('index', {title: 'Success', successMsg: 'Welcome to your profile!'});
 });
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
 
 module.exports = function(passport) {
     router.post('/signup', passport.authenticate('local-signup', {
