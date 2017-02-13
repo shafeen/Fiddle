4faad5a4da534530ccc9bdc5435585660727a484
shafeen <sodl02@gmail.com>
2017-02-13 17:35:27 -0500

    updated ng-component-test: still trying to get nested components to work

---
+++ Javascript/ng-component-test/public/angular/card-grid/card-grid.component.js
@@ -0,0 +1,39 @@
+(function() {
+    let app = angular.module('cardApp', []);
+
+    function CardGridCtrl() {
+        let ctrl = this;
+        
+        // This data would normally be retrieved via a service
+        ctrl.$onInit = function () {
+            ctrl.cards = [
+                {
+                    cost: 3,
+                    title: 'Reclusiam Templars',
+                    imgUrl: 'http://s3.amazonaws.com/conquestdb2/image/card/en/12-wotc/094-reclusiam-templars.jpg',
+                    traits: ['Soldier', 'Black Templars']
+                },
+                {
+                    cost: 5,
+                    title: 'The Emperor\'s Champion',
+                    imgUrl: 'http://s3.amazonaws.com/conquestdb2/image/card/en/12-wotc/095-the-emperors-champion.jpg',
+                    traits: ['Soldier', 'Black Templars', 'Elite']
+                },
+                {
+                    cost: 1,
+                    title: 'Vow of Honor',
+                    imgUrl: 'http://s3.amazonaws.com/conquestdb2/image/card/en/12-wotc/097-vow-of-honor.jpg',
+                    traits: ['Power', 'Vow']
+                }
+            ];
+            console.log('Card grid initialized with cards: ', ctrl.cards);
+
+        };
+    }
+    
+    app.component('cardGrid', {
+        templateUrl: '/angular/card-grid/card-grid.component.html',
+        controllerAs: 'ctrl',
+        controller: CardGridCtrl
+    });
+}());
---
+++ Javascript/ng-component-test/public/angular/single-card/single-card.component.js
@@ -0,0 +1,29 @@
+(function() {
+    let app = angular.module('cardApp', []);
+    
+    function CardCtrl() {
+        let ctrl = this;
+
+        ctrl.$onInit = function () {
+            console.log('card "%s" with cost %d initialized', ctrl.title, ctrl.cost);
+        };
+        
+        ctrl.cost = 0;
+        ctrl.title = 'Card title';
+        ctrl.imgUrl = 'http://s3.amazonaws.com/conquestdb2/image/card/en/12-wotc/094-reclusiam-templars.jpg';
+        ctrl.traits = [];
+        ctrl.description = '(description goes here)';
+    }
+
+    app.component('singleCard', {
+        templateUrl: '/angular/single-card/single-card.component.html',
+        transclude: true,
+        bindings: {
+            title: '@',
+            cost: '<',
+            imgUrl: '@',
+            traits: '<'
+        },
+        controller: CardCtrl
+    });
+}());
