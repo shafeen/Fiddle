3d045164671c8380d3fe5d4da53624ff460800d8
Shafeen M <sodl02@gmail.com>
2017-09-25 18:16:38 -0400

    updated alphavantage-quotes-lambda to be able to send AWS SNS messages note that certain environmental variables are necessary

--- Python/alphavantage-quotes-lambda/historicalData.py
+++ Python/alphavantage-quotes-lambda/historicalData.py
@@ -1,5 +1,6 @@
 import os
 
+import boto3
 from datetime import datetime
 
 import requests
@@ -27,13 +28,37 @@ def get_last_week_closing_prices(symbol):
 
     endOfWeekDiff = round((lastWeekClosingPrices[-1] - lastWeekClosingPrices[0])*100/lastWeekClosingPrices[0], 2)
     diffStr = '+'+str(endOfWeekDiff)+'%' if endOfWeekDiff >= 0 else str(endOfWeekDiff)+'%'
-    print symbol, diffStr, lastWeekClosingPrices
+    summaryMsg = symbol+' '+diffStr+' '+lastWeekClosingPrices.__str__()
+    print summaryMsg
+    return summaryMsg
+
+
+def send_sns_message(subject, message, sns_topic_region, sns_topic_arn):
+    # remember to set the default region, and aws access
+    # keys and secrets beforehand (use AWS CLI)
+    client = boto3.client('sns', region_name=sns_topic_region)
+    client.publish(
+        TopicArn=sns_topic_arn,
+        Message=message,
+        Subject=subject,
+    )
 
 
 def lambda_handler(event, context):
     stockSymbols = os.environ['symbols'].split(',')
-
+    lastWeekStockSummaries = 'Last week\'s stock performance:'
     for symbol in stockSymbols:
-        get_last_week_closing_prices(symbol)
+        lastWeekStockSummaries += '\n'+ get_last_week_closing_prices(symbol)
+
+    sns_topic_region = os.environ['sns_topic_region']
+    sns_topic_arn = os.environ['sns_topic_arn']
+    send_sns_message(
+        'Last week\'s stock performance',
+        lastWeekStockSummaries,
+        sns_topic_region,
+        sns_topic_arn
+    )
+
 
 # lambda_handler('test', 'test')
+# send_sns_message('test1', 'testmessage here')
