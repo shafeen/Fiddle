5095e6bbf66c5fa79ccedb96d4685851496c6bed
Shafeen M <sodl02@gmail.com>
2018-08-17 14:10:45 -0400

    added a python test project "module-organization"

---
+++ Python/module-organization/main.py
@@ -0,0 +1,6 @@
+from utilities import mathutil
+
+if __name__=='__main__':
+    print('running main.py')
+    print (mathutil.multiply(12,12))
+
---
+++ Python/module-organization/utilities/mathutil.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a+b
+
+def multiply(a, b):
+    return a*b
+
+def divide(numerator=1, denominator=1):
+    return numerator/denominator
+
