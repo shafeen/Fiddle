7f5b1b3cbff22c275c4b32795a23876e827d7c2b
Shafeen M <sodl02@gmail.com>
2018-09-15 02:09:22 -0400

    updated graphql-basics: got ApolloBoost working in the client

--- Javascript/graphql-basics/gulpfile.js
+++ Javascript/graphql-basics/gulpfile.js
@@ -4,31 +4,23 @@ const log = require('gulplog');
 const tap = require('gulp-tap');
 const buffer = require('gulp-buffer');
 const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
+const uglifyEs = require('gulp-uglify-es').default;
 
 gulp.task('js', function () {
 
-    return gulp.src('./public/**/*.js', {read: false}) // no need of reading file because browserify does.
+    return gulp.src('./public/js/*.js', {read: false}) // no need of reading file because browserify does.
         // transform file objects using gulp-tap plugin
         .pipe(tap(function (file) {
-
             log.info('bundling ' + file.path);
-
             // replace file contents with browserify's bundle stream
             file.contents = browserify(file.path, {debug: true}).bundle();
-
         }))
-
         // transform streaming contents into buffer contents (because gulp-sourcemaps does not support streaming contents)
         .pipe(buffer())
-
         // load and init sourcemaps
         .pipe(sourcemaps.init({loadMaps: true}))
-
-
-        // write sourcemaps
-        .pipe(sourcemaps.write('./'))
-
+        // .pipe(uglifyEs())
+        // write sourcemaps to path relative to destination
+        // .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./public/build'));
-
 });
--- Javascript/graphql-basics/public/js/script.js
+++ Javascript/graphql-basics/public/js/script.js
@@ -10,3 +10,22 @@ function testFunction() {
     console.log('this is just a test javascript function');
 }
 testFunction();
+
+function runQuery(client) {
+    const gql = ApolloBoost.gql;
+
+    client.query({
+        query: gql`
+            query AllBooks {
+                books {
+                    author
+                    title
+                }
+            }
+        `
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}
+
+runQuery(client);
